a1da57823162c11ced509335c923585c51371c1b
GodFather <1845727893@qq.com>
2019-05-20 21:35:31 +0800

    未完成:提交分类

---
+++ application/admin/controller/Aericle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 19/5/20
+ * Time: 下午8:03
+ */
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Aericle extends Controller
+{
+    //
+}
---
+++ route/route.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+//用户登录的路由
+Route::rule('login', 'admin/Login/in')->method('GET,POST');
+
+//后台首页
+Route::get('admin$', 'admin/Index/index');
+
+//console页
+Route::get('admin-console', 'admin/Index/console');
+Route::rule('admin-add-category', 'admin/Index/addCategory')->method('GET,POST');
+Route::get('admin-list-category', 'admin/Index/categoryList');
