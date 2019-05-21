664e1a012593fd7022e7213f6ccdcb609710623b
爸爸 <1845727893@qq.com>
2019-05-21 16:17:19 +0800

    已完成:无法提交分类

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 19/5/20
+ * Time: 下午2:46
+ */
+
+namespace app\admin\controller;
+
+use app\admin\model\category;
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function console()
+    {
+        return $this->fetch();
+    }
+
+
+    public function addCategory()
+    {
+
+        $re = $this->request;
+
+        if($re->isGet()){
+            $pid = $re->param('id', 0);
+
+            if (empty($pid)){
+
+                $this->assign('parentName', '顶级分类');
+            }else{
+                $parentName = category::where('id', $pid)->value('name');
+                if (!$parentName){
+                    $this->error('非法操作');
+                }
+                $this->assign('parentName', $parentName);
+            }
+            $this->assign('pid', $pid);
+            return $this->fetch();
+        }
+
+        if ($re->isPost()){
+
+            $name = $re->param('name');
+            $pid = $re->param('pid', 0);
+
+            if (mb_strlen($name, 'utf-8') > 10 || mb_strlen($name, 'utf-8') < 2){
+                $this->error('分类名称长度应在2-10位之间');
+            }
+            //同一个父级下不能重名
+            $where = ['pid'=>$pid, 'name'=>$name];
+            if (category::where($where)->find()){
+                $this->error('该分类已存在');
+            }
+
+<<<<<<< HEAD
+            if ($pid == 0){
+
+                //顶级分类的处理
+                $level = 0;
+                $path = '0-';
+            }else{
+                $parent = category::where('id', $pid)->find();
+                if (empty($parent)){
+
+                    //如果通过用户穿的pid找不到信息，说明是一个非法操作
+                    $this->error('非法操作');
+                }
+                $level = $parent->level + 1;
+                $path = $parent->path .$pid.'-';
+
+            }
+=======
+            $parent = category::where('id', $pid)->find();
+>>>>>>> a1da57823162c11ced509335c923585c51371c1b
+
+            //入库
+            $data = [
+                'name'=>$name,
+                'pid' => $pid,
+<<<<<<< HEAD
+                'level' =>$level,
+                'path' => $path
+=======
+                'level' => $parent->level + 1
+>>>>>>> a1da57823162c11ced509335c923585c51371c1b
+            ];
+            if (category::create($data)){
+                $this->success('成功');
+            }else{
+                $this->error('失败');
+            }
+        }
+    }
+
+    public function categoryList()
+    {
+
+        //如果使用的是ajax请求
+        if ($this->request->isAjax()){
+            $pid = $this->request->param('id', 0);
+            $list = category::where('pid', $pid)->select();
+
+            $str = '';
+            foreach ($list as $v){
+                $space = '';
+                for ($i=0; $i< $v['level']; $i++){
+                    $space .= '&nbsp;&nbsp;&nbsp;&nbsp;';
+                }
+                $url = url('admin/Index/addCategory', ['id'=>$v['id']]);
+                $str .= <<<DDDD
+                    <tr class="x{$pid}">
+                        <td>{$v['id']}</td>
+                        <td>{$space}|--{$v['name']}</td>
+                        <td><a href="{$url}">添加</a></td>
+                        <td><a data-id="{$v['id']}" class="point-e children" data-op="plus"><i class="fa fa-plus"></i></a></td>
+                    </tr>
+DDDD;
+            }
+
+            return $str;
+        }else{
+            $list = category::where('pid', 0)->select();
+            $this->assign('list', $list);
+            return $this->fetch();
+        }
+    }
+
+}
---
+++ application/admin/model/article.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 19/5/20
+ * Time: 下午8:01
+ */
+
+
+namespace app\admin\model;
+
+use think\Model;
+
+class article extends Model
+{
+    //
+}
